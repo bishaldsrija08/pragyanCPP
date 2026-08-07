64021b24015793b1928a6f35d849533b871f669a
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-08-07 20:33:16 +0545

    Refactor code structure for improved readability and maintainability

---
+++ conditional statement/oet.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // string result;
+    int num=4;
+    cout << ((num%2==0)? "Even number": "Odd number");
+    // cout << result;
+    return 0;
+}
---
+++ conditional statement/ter.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // string result;
+    int time = 2;
+    cout << ((time < 18)? "Good day.": "Good evening.");
+    // cout << result;
+    return 0;
+}
