db29bf12c218ed7055fd8b839c8eb3da36a6a3b5
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-08-27 20:35:28 +0545

    task

---
+++ Looping/extra/revise.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    Syntax of for loop in C++:
+    for(initialization; condition; increment/decrement){
+        // code to be executed
+    }
+
+    syntax of while loop in C++:
+    while(condition){
+        // code to be executed
+    }
+
+    // syntax of do while loop in C++:
+    do{
+        // code to be executed
+    }while(condition);
+
+    */
+}
