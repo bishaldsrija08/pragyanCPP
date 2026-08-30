c298c7eb769d07afc25a1d7ec365320f3dd36ecf
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-08-30 20:42:07 +0545

    Implement code changes to enhance functionality and improve performance

---
+++ Functions/scopee.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+string name = "Bishal Rijal";
+
+int hahahehe(){
+    string name = "Bishal Rijal is hero";
+    int apple = 67;
+    cout << name << "asko" << endl;
+    return 6;
+}
+
+int main(){
+    cout << hahahehe() << endl;
+    // cout << apple << endl;
+    cout << name << endl;
+    return 0;
+}
